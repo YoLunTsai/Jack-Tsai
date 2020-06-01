3f5a53a305934c3f7b6603e69e05878bfee6d21f
YoLunTsai <qaz6579@yahoo.com.tw>
2020-06-01 22:43:20 +0800

    網頁icon調整、新增main區塊文字動畫

--- js/app.js
+++ js/app.js
@@ -14,3 +14,22 @@ function show(item) {
     show.classList.toggle("hide");
     show.scrollIntoView(true);
 }
+
+// 文字動畫效果
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+anime.timeline({
+        loop: true
+    })
+    .add({
+        targets: '.ml10 .letter',
+        rotateY: [-90, 0],
+        duration: 1500,
+        delay: (el, i) => 45 * i
+    }).add({
+        targets: '.ml10',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 3500
+    });
